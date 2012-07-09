1e61dd2068d4e6afbf7887ea80069a1cb03eb5b3
drakontia <mhden@drakontia.com>
2012-07-10 02:32:55 +0900

    Settingページの設定

---
+++ app/models/sphinx_pages_project_setting.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+class SphinxPagesProjectSetting < Settingslogic
+  unloadable
+  include Redmine::SafeAttributes
+
+  belongs_to :project
+
+  validates_presence_of :project_id
+
+  before_save :set_assignment_settings
+
+  def self.find_or_create(project)
+    setting = SphinxPagesProjectSetting.find(:first, :conditions => ['project_id = ?',  project.id])
+    unless setting
+      setting = SphinxPagesProjectSetting.new
+      setting.project_id = project_id
+      setting.save!
+    end
+    setting
+  end
+
+end
+
--- init.rb
+++ init.rb
@@ -3,19 +3,21 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_sphinx_pages do
   name 'Redmine Sphinx Pages plugin'
-  author 'Daisuke Miura'
+  author 'nishio-dens, drakontia'
   author_url 'http://twitter.com/drakontia'
   description 'This is a plugin for Redmine'
   version '0.0.2'
   requires_redmine :version_or_higher => '2.0.0'
 
-  # top menuに追加
-  #  menu :top_menu, :sphinx_list, { :controller => 'sphinx', :action => 'index' }, :caption => "List", :last => true
-
   # sphinx用のメニュー追加
   project_module :sphinx_pages do
-    permission :sphinx, {:sphinx => [:index]}, :public => true
+    permission :view_sphinx_pages, {:sphinx => [:index]}, :public => true
+    permission :config_sphinx_pages,  {:config_sphinx_pages => [:show,  :update,  :add_filter,  :edit_filter,  :sort]},  :require => :member
   end
 
-  menu :project_menu, :sphinx_list, { :controller => 'sphinx', :action => 'index' }, :caption => 'Sphinx', :param => :project_id
+  menu :project_menu, :sphinx_pages, { :controller => 'sphinx', :action => 'index' }, :caption => 'Sphinx',
+    :if => Proc.new{|project|
+    setting = SphinxPagesProjectSetting.find_or_create(project)
+    project.repository != nil and setting adn !setting.hide_sphinx_pages_tab
+  }, :after => :repository, :param => :project_id
 end
